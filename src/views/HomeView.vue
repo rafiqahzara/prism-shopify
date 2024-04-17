c41ace2dc9d3a49d2afe56f6ebbc02ff2a1d48a6
Rafiqah Zara <rafiqah.rahmat@prodigious.com>
2024-04-17 15:00:40 +0800

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+
+const carModels = [
+  {
+    img: "/images/1-chile-red.png",
+    thumbnail: "/images/1-chile-red.webp",
+    name: "Chile Red"
+  },
+
+  {
+    img: "/images/2-sage-green.png",
+    thumbnail: "/images/2-sage-green.webp",
+    name: "Sage Green"
+  },
+]
+</script>
+
+<template>
+  <v-carousel>
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      cover></v-carousel-item>
+
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+      cover></v-carousel-item>
+
+    <v-carousel-item
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      cover></v-carousel-item>
+  </v-carousel>
+</template>
